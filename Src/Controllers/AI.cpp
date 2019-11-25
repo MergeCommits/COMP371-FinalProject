f86dc91e2534b44b8a704a8c00be2b85d1eba415
CommanderMark <markjarjour@gmail.com>
2019-11-24 22:17:12 -0500

    Added random movement to AI. Cleanup code.

--- Src/Controllers/AI.cpp
+++ Src/Controllers/AI.cpp
@@ -1,11 +1,11 @@
 #include <cmath>
 
 #include "AI.h"
-#include "../Objects/Car.h"
 
 AI::AI(Shader* shd, Shader* colliderShd) {
     car = new Car(shd, colliderShd);
     stepsUntilNewTask = 0;
+    currTaskDirection = Car::WalkInput::None;
 }
 
 AI::~AI() {
@@ -20,27 +20,51 @@ Car* AI::getCar() const {
     return car;
 }
 
-void AI::update(float timestep) {
-    Car::WalkInput input = Car::WalkInput::Forward;
-    
-    float MIN_DISTANCE_TO_ORIGIN_BEFORE_RANDOM_MOVEMENT_SQUARED = 20.f * 20.f;
+Car::WalkInput AI::getCurrentTaskDirection() {
+    if (currTaskDirection == Car::WalkInput::None) {
+        updateCurrentTaskDirection();
+    }
+    return currTaskDirection;
+}
+
+void AI::updateCurrentTaskDirection() {
+    currTaskDirection = (std::rand() % 2) == 0 ? Car::WalkInput::Left : Car::WalkInput::Right;
+}
+
+void AI::updateFarInput(Car::WalkInput& input) {
     float MIN_ANGLE_TO_REACH_ORIGIN = 0.1f;
-    float distanceToOriginSquared = car->getPosition().lengthSquared();
     Vector2f vectorFromOrigin = Vector2f(car->getPosition().x, car->getPosition().z);
     
     float sinAngle = std::sin(car->getRotationY());
     float cosAngle = std::cos(car->getRotationY());
     float differenceBetweenOriginAndCar = Vector2f(sinAngle, cosAngle).normalize().dotProduct(vectorFromOrigin.normalize());
     
-    if (distanceToOriginSquared > MIN_DISTANCE_TO_ORIGIN_BEFORE_RANDOM_MOVEMENT_SQUARED) {
-        // Far away -> Try to get closer to origin.
-        if (differenceBetweenOriginAndCar > (-1.f + MIN_ANGLE_TO_REACH_ORIGIN)) {
-            // TODO: Figure out optimal turn direction.
-            input = input | Car::WalkInput::Left;
-        }
-    } else {
-        // Close -> Start random movement.
+    if (differenceBetweenOriginAndCar > (-1.f + MIN_ANGLE_TO_REACH_ORIGIN)) {
+        input = input | getCurrentTaskDirection();
         
+    }
+}
+
+void AI::updateNearInput(Car::WalkInput& input) {
+    int STEPS_WITHOUT_NEW_TASK = 10;
+    if (stepsUntilNewTask < -STEPS_WITHOUT_NEW_TASK) {
+        stepsUntilNewTask = (std::rand() % 30) + 1;
+        updateCurrentTaskDirection();
+    }
+    
+    stepsUntilNewTask--;
+    input = input | getCurrentTaskDirection();
+}
+
+void AI::update(float timestep) {
+    float MIN_DISTANCE_TO_ORIGIN_BEFORE_RANDOM_MOVEMENT_SQUARED = 20.f * 20.f;
+    float distanceToOriginSquared = car->getPosition().lengthSquared();
+    Car::WalkInput input = Car::WalkInput::Forward;
+    
+    if (distanceToOriginSquared > MIN_DISTANCE_TO_ORIGIN_BEFORE_RANDOM_MOVEMENT_SQUARED) {
+        updateFarInput(input);
+    } else {
+        updateNearInput(input);
     }
     
     car->update(input, timestep);
--- Src/Controllers/AI.h
+++ Src/Controllers/AI.h
@@ -1,13 +1,15 @@
 #ifndef AI_H_INCLUDED
 #define AI_H_INCLUDED
 
-class Car;
+#include "../Objects/Car.h"
+
 class Shader;
 
 class AI {
 private:
     Car* car;
     
+    Car::WalkInput currTaskDirection;
     int stepsUntilNewTask;
 
 public:
@@ -17,6 +19,11 @@ public:
     void setCarShader(Shader* shd);
     Car* getCar() const;
     
+    Car::WalkInput getCurrentTaskDirection();
+    void updateCurrentTaskDirection();
+    
+    void updateFarInput(Car::WalkInput& input);
+    void updateNearInput(Car::WalkInput& input);
     void update(float timestep);
     void render() const;
 };
